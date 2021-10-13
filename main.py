e4ba9c5826c16768b1986f64551b5b27822d9506
prajith2252 <83197685+prajith2252@users.noreply.github.com>
2021-10-13 12:42:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -79,9 +79,9 @@ async def text_handler(_, m: Message):
                 break
             count += 1
             text += f"**Title:** `{data[i]['title']}`\n" \
-                    f"**Description:** `{data[i]['description']}`\n" \
+                    f"**Description: @tmamovies** `{data[i]['description']}`\n" \
                     f"**PDisk Link:** {Configs.PDISK_DOMAIN + 'share-video?videoid=' + data[i]['share_link'].split('=', 1)[-1]}\n\n"
-                     f"**A bot made by @tmamovies \n\n"
+                    
         try: await editable.edit(text, disable_web_page_preview=True)
         except MessageNotModified: pass
 
